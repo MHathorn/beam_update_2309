cd0a9bb6238940fc1f1a19f8ead9bb37267e4828
Nathan Kiner <nathankiner@gmail.com>
2023-11-24 14:55:49 +0000

    Relocated training codefiles

---
+++ beam_model_training/training/losses.py
@@ -0,0 +1,52 @@
+
+from fastai.vision.all import FocalLossFlat, DiceLoss, torch, nn, store_attr
+
+
+# Custom loss functions
+class CombinedLoss:
+    """
+    Dice and Focal combined
+    """
+
+    def __init__(self, axis=1, smooth=1., alpha=1.):
+        store_attr()
+        self.focal_loss = FocalLossFlat(axis=axis)
+        self.dice_loss = DiceLoss(axis, smooth)
+
+    def __call__(self, pred, targ):
+        return self.focal_loss(pred, targ) + self.alpha * self.dice_loss(pred, targ)
+
+    def decodes(self, x):    return x.argmax(dim=self.axis)
+
+    def activation(self, x): return F.softmax(x, dim=self.axis)
+
+
+class DualFocalLoss(nn.Module):
+    """
+    This loss is proposed in this paper: https://arxiv.org/abs/1909.11932
+    """
+
+    def __init__(self, ignore_lb=255, eps=1e-5, reduction='mean'):
+        super(DualFocalLoss, self).__init__()
+        self.ignore_lb = ignore_lb
+        self.eps = eps
+        self.reduction = reduction
+        self.mse = nn.MSELoss(reduction='none')
+
+    def forward(self, logits, label):
+        ignore = label.data.cpu() == self.ignore_lb
+        n_valid = (ignore == 0).sum()
+        label = label.clone()
+        label[ignore] = 0
+        lb_one_hot = logits.data.clone().zero_().scatter_(1, label.unsqueeze(1), 1).detach()
+
+        pred = torch.softmax(logits, dim=1)
+        loss = -torch.log(self.eps + 1. - self.mse(pred, lb_one_hot)).sum(dim=1)
+        loss[ignore] = 0
+        if self.reduction == 'mean':
+            loss = loss.sum() / n_valid
+        elif self.reduction == 'sum':
+            loss = loss.sum()
+        elif self.reduction == 'none':
+            loss = loss
+        return loss
--- beam_model_training/training/training_functions.py
+++ beam_model_training/training/training_functions.py
@@ -1,7 +1,6 @@
 from fastai.vision.all import *
 import numpy as np
 import random
-import os
 import pytz
 from datetime import datetime
 from semtorch import get_segmentation_learner
@@ -11,55 +10,6 @@ from os.path import join
 
 import utils.my_paths as p
 
-
-# Custom loss functions
-class CombinedLoss:
-    """
-    Dice and Focal combined
-    """
-
-    def __init__(self, axis=1, smooth=1., alpha=1.):
-        store_attr()
-        self.focal_loss = FocalLossFlat(axis=axis)
-        self.dice_loss = DiceLoss(axis, smooth)
-
-    def __call__(self, pred, targ):
-        return self.focal_loss(pred, targ) + self.alpha * self.dice_loss(pred, targ)
-
-    def decodes(self, x):    return x.argmax(dim=self.axis)
-
-    def activation(self, x): return F.softmax(x, dim=self.axis)
-
-
-class DualFocalLoss(nn.Module):
-    """
-    This loss is proposed in this paper: https://arxiv.org/abs/1909.11932
-    """
-
-    def __init__(self, ignore_lb=255, eps=1e-5, reduction='mean'):
-        super(DualFocalLoss, self).__init__()
-        self.ignore_lb = ignore_lb
-        self.eps = eps
-        self.reduction = reduction
-        self.mse = nn.MSELoss(reduction='none')
-
-    def forward(self, logits, label):
-        ignore = label.data.cpu() == self.ignore_lb
-        n_valid = (ignore == 0).sum()
-        label = label.clone()
-        label[ignore] = 0
-        lb_one_hot = logits.data.clone().zero_().scatter_(1, label.unsqueeze(1), 1).detach()
-
-        pred = torch.softmax(logits, dim=1)
-        loss = -torch.log(self.eps + 1. - self.mse(pred, lb_one_hot)).sum(dim=1)
-        loss[ignore] = 0
-        if self.reduction == 'mean':
-            loss = loss.sum() / n_valid
-        elif self.reduction == 'sum':
-            loss = loss.sum()
-        elif self.reduction == 'none':
-            loss = loss
-        return loss
 
 
 # Set path of root folder of images and masks
