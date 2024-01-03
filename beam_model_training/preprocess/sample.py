e408a30d7dc9a08782b06c278f2c11288c029d8f
Nathan Kiner <nathankiner@gmail.com>
2024-01-03 15:32:49 +0000

    updated sample to create png files

--- beam_model_training/preprocess/sample.py
+++ beam_model_training/preprocess/sample.py
@@ -1,10 +1,14 @@
-import shutil
+from pathlib import Path
+
 import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import rioxarray as rxr
-from pathlib import Path
+from preprocess.transform import get_rgb_channels
 from shapely.geometry import box
-from utils.helpers import create_if_not_exists
+from utils.helpers import create_if_not_exists, seed
+
 
 def load_tile_info(tile_path):
     """Load tile and extract metadata."""
@@ -15,8 +19,13 @@ def load_tile_info(tile_path):
 
 def tile_in_settlement(tile, settlements):
     """Check if the tile overlaps with any informal settlement."""
+    
+    # Ensure both geometries are in the same CRS
+    if tile.rio.crs != settlements.crs:
+        tile = tile.rio.reproject(settlements.crs)
+        
     tile_geom = box(*tile.rio.bounds())
-    return settlements.intersects(tile_geom).any()
+    return any(settlements.intersects(tile_geom))
 
 def calculate_score(num_buildings, probability_score, in_settlement):
     """Calculate a score for the tile based on the given criteria."""
@@ -50,9 +59,19 @@ def sample_tiles(tile_directory, settlements_shapefile, sample_size):
 
 # Example usage
 if __name__ == '__main__':
+    seed(2022)
     input_dir = Path("C:/Users/natha/OneDrive/Desktop/Local_Training/tiles/images")
-    output_dir = create_if_not_exists(input_dir.parent / "sample")
+    output_dir = create_if_not_exists(input_dir.parent / "sample_png")
     sampled_tile_paths = sample_tiles("C:/Users/natha/OneDrive/Desktop/Local_Training/tiles/images", "C:/Users/natha/OneDrive/Desktop/Local_Training/Costa_Rica/PR05_Asentamientos_Informales_MIVAH_2023_2.shp", 20)
     print(sampled_tile_paths)
     for file_path in sampled_tile_paths:
-        shutil.copy2(file_path, output_dir / file_path.name)
+        tiff_file = Path(file_path)
+        png_file = output_dir / tiff_file.with_suffix('.png').name
+
+    # Open the TIFF file and convert it to PNG
+        try:
+            img = get_rgb_channels(tiff_file)
+            plt.imsave(png_file, np.transpose(img.values, (1,2,0)))   
+            print(f"Converted {tiff_file} to {png_file}")
+        except Exception as e:
+            print(f"An error occurred while converting {tiff_file}: {e}")
